cbd19fce0b4a30bedadb98ee0957dd192e74e8ea
bullets012 <lancegenesismercado0111@gmail.com>
2024-09-26 13:10:25 +0800

    Insight

--- header.js
+++ header.js
@@ -2,8 +2,15 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 100) {
     header.classList.add("dark");
-  }
-  else{
+  } else {
     header.classList.remove("dark");
   }
+});
+
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__nav");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
 });
